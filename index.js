2ffc06031698d04083f5a4139c93853e10231331
Jounicode <96347521+Jounicrypto@users.noreply.github.com>
2022-08-20 09:40:51 +0200

    m

---
+++ index.js
@@ -0,0 +1,70 @@
+//before ES6 var
+// var name = 'Ehsan' ;
+// var lastName = 'Jorj';
+
+//after ES6 let const
+//let
+// let age = 30;
+// age = 20 ;
+//console.log(age)
+
+//const
+// const number = 5;
+// number = 6;
+
+// console.log(number);
+
+//limitation for variable name :
+//1)reserve keys like let const for true ,....
+//2) do not start with numbers 
+//3) no space between word
+//4)case sensitive
+
+// let firstName = 'Ehsan'; //String "" ''
+// let age = 30 ; //number
+// let isAdmin = true ; //Boolean true/false
+// let lastName ; //undefined
+// let number = null ; //null
+
+// console.log(number);
+// console.log(c);
+
+// let user = {
+//     firstName : "Shayan",
+//     lastName : "Zarei",
+//     age : 24 ,
+//     isAdmin : true ,
+//     hobby : ["travelling","gaming" ,"coding",20,true , ""] 
+// }
+
+// //dot notation
+// //console.log(user.hobby);
+
+// //brackets notation
+// console.log(user["hobby"].length);
+
+// function sayHello(first, last) {
+//     console.log("hello" + " " + first + " " + last);
+//     console.log(`hello ${first} ${last}`);
+// }
+
+// sayHello('Shayan2', 'Zarei2');
+
+// function addNumber (number){
+//     return (number+number);
+// }
+
+// console.log(addNumber(20)) ;
+
+let x = 6;
+//x = x+6
+//x += 6
+
+//x=x-5
+//x -= 5
+
+// x*=
+// x/=
+// x%=
+
+console.log(x);
